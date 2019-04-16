0c7ffcc9157ff130a881428df6a3c77de0cf6634
Elison Bruno <elisonbrunosilva@gmail.com>
2019-04-16 10:26:58 -0300

    upload files

---
+++ assets/inc/js.php
@@ -0,0 +1,4 @@
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<script src="assets/bootstrap/javascripts/bootstrap.min.js"></script>
+<script src="assets/js/smoothscroll.min.js"></script>
+<script src="assets/js/main.js"></script>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR" x>
+  <head>
+    <!-- Metas -->
+    <?php include "assets/inc/metas.php"; ?>
+
+    <!-- Favicon -->
+    <?php include "assets/inc/favicon.php"; ?>
+
+    <title>Title</title>
+    
+    <!-- Head -->
+    <?php include "assets/inc/head.php"; ?>
+  </head>
+  <body>
+    <header>
+      Header
+    </header>
+
+    <main>
+      Main
+    </main>
+
+    <footer>
+    	Footer
+    </footer>
+    <!-- Page Js -->
+    <?php include "assets/inc/js.php"; ?>
+  </body>
+</html>
